7839a7f55c0f95f256c601537a0ced0b376c44ee
Jonas Andersson <jonasanderssonweb@gmail.com>
2016-09-28 15:38:05 +0200

    Added database script and modified gitignore

---
+++ customer/scripts/db/db.php
@@ -0,0 +1,41 @@
+<?php
+
+error_reporting(-1);
+
+$string = file_get_contents("preferences.json");
+$preferences = json_decode($string, true);
+
+$connection = mssql_connect($preferences['host'], $preferences['user'], $preferences['password']);
+mssql_select_db($preferences['database'], $connection);
+$sql = "";
+
+//////////////////////////////////////////////
+/////// Set up statements for input
+//////////////////////////////////////////////
+
+
+
+//////////////////////////////////////////////
+/////// End statements for input
+//////////////////////////////////////////////
+
+$result = [];
+$query = mssql_query($sql);
+
+if(mssql_num_rows($query)){
+  while($row = mssql_fetch_array($query, MSSQL_ASSOC)){
+    $result[] = $row;
+    if(isset($_GET['verbose']) && $_GET['verbose'] == True){
+      echo "<pre>";
+      var_dump($row);
+      echo "</pre>";
+    }
+  }
+}
+
+
+header('Content-type: application/json');
+echo json_encode($result);
+
+mssql_free_result($query);
+mssql_close($connection);
